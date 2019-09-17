27db3253132251cbaa2f09156f5810a56004796d
w-wanxing <2754456500@qq.com>
2019-09-17 21:13:31 +0800

    commit

---
+++ src/abc/Single.java
@@ -0,0 +1,18 @@
+package abc;
+
+class Single {
+		private static Single INSTANCE=new Single ();
+		private Single(){}
+		static Single getInstance(){
+		return INSTANCE;
+		}
+}
+class Example15{
+		public static void main(String[] args){
+		Single s1= Single.getInstance();
+		Single s2= Single.getInstance();
+		System.out.println(s1==s2);
+		}
+		
+
+}
